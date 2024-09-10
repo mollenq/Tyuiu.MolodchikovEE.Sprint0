2954e7308c0c4d9eb9d0f307f598040013665272
Mollenq <Mollenq@DESKTOP-5H39EA1>
2024-09-10 21:28:08 +0500

    Add project files.

---
+++ Tyuiu.MolodchikovEE.Sprint0.Task0.V1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.MolodchikovEE.Sprint0.Task0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
