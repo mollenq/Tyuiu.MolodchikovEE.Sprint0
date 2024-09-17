3eb4415c345669ee307e0a87d04ba9120f307385
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-17 19:29:49 +0500

    Task1

---
+++ Tyuiu.MolodchikovEE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.MolodchikovEE.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Егор";
+            int age = 20;
+            Console.WriteLine("Здравствуйте, меня зовут "+name+" мне "+age+" лет!");
+            Console.ReadKey();
+        }
+    }
+}
