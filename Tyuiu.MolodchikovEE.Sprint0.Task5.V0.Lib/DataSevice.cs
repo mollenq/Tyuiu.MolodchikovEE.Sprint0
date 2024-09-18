2762dfd34ab4daea0f41b0f38af47c01ec200224
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-18 23:39:15 +0500

    Task5

---
+++ Tyuiu.MolodchikovEE.Sprint0.Task5.V0.Lib/DataSevice.cs
@@ -0,0 +1,27 @@
+﻿namespace Tyuiu.MolodchikovEE.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        { return a + b; }
+
+        public static int Subtraction(int a, int b)
+        { return a - b; }
+
+        public static int Multiplication(int a, int b)
+        { return a * b; }
+
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b=0, на ноль делить нелья", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
